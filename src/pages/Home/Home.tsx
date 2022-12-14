ce742f95e31c4110748a1af371fe0170d652fdf4
jagtym <jagtymxd@gmail.com>
2022-12-14 23:44:09 +0100

    Add homepage property in package.json

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import { FaReact } from 'react-icons/fa'
 
-function Home() {
-  return (
-    <div className="flex flex-col justify-center h-screen align-middle">
-      <div className="flex flex-row justify-center text-[10rem] motion-safe:animate-spin-slow text-blue-600">
-        <FaReact />
-      </div>
-      <h1 className="text-primary text-center">Example App</h1>
+const Home = () => (
+  <div className="flex flex-col justify-center h-screen align-middle">
+    <div className="flex flex-row justify-center text-[10rem] motion-safe:animate-spin-slow text-blue-600">
+      <FaReact />
     </div>
-  )
-}
+    <h1 className="text-primary text-center">Example App</h1>
+  </div>
+)
 export default Home
