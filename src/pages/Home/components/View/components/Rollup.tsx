9a526140a7ac6f9441fe30ef077503ad6ef3c889
MChlebosz <mateusz.chlebosz@outlook.com>
2022-12-17 18:58:23 +0100

    Animated rollup

--- src/pages/Home/components/View/components/Rollup.tsx
+++ src/pages/Home/components/View/components/Rollup.tsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaArrowCircleUp, FaArrowCircleDown } from 'react-icons/fa'
 
-const Rollup = () => <div className="h-10 bg-pink-200  w-full">Rollup</div>
+const Rollup = () => {
+  const [expand, setExpand] = useState<boolean>(false)
+  let className = 'bg-pink-200  w-full transition-all ease-in-out'
+  if (expand) {
+    className += ' h-[20rem] max-h-[90vh]'
+  } else {
+    className += ' h-10'
+  }
+  return (
+    <div className={className}>
+      <div className="relative flex flex-col justify-start">
+        <div
+          role="presentation"
+          className="bg-pink-200 rounded-full p-2 absolute top-0 left-1/2 transform -translate-y-1/2 hover:cursor-pointer"
+          onClick={() => setExpand(!expand)}
+        >
+          <div className={expand ? 'hidden' : ''}>
+            <FaArrowCircleUp />
+          </div>
+          <div>
+            <FaArrowCircleDown className={expand ? '' : 'hidden'} />
+          </div>
+        </div>
+        Rollup
+      </div>
+    </div>
+  )
+}
 
 export default Rollup
